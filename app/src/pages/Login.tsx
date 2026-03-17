03e2ea2e3856fb086e13330bb233cdd227d9e06a
RenisideOfficial <renrenxp1@gmail.com>
2026-03-18 00:28:20 +0100

    feat: init

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,18 +1,18 @@
 import {
-	Flex,
-	Box,
-	FormControl,
-	FormLabel,
-	Input,
-	Stack,
-	Button,
-	Heading,
-	Text,
-	InputGroup,
-	InputRightElement,
-	Link,
-	HStack,
-	useToast,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  InputGroup,
+  InputRightElement,
+  Link,
+  HStack,
+  useToast,
 } from "@chakra-ui/react";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import React, { useEffect, useLayoutEffect, useRef } from "react";
@@ -23,119 +23,118 @@ import { Turnstile, TurnstileInstance } from "@marsidev/react-turnstile";
 import { useReducer } from "react";
 
 export default function Login() {
-	const toast = useToast();
-	const navigate = useNavigate();
+  const toast = useToast();
+  const navigate = useNavigate();
 
-	const turnstileRef = useRef<TurnstileInstance>(null);
+  const turnstileRef = useRef<TurnstileInstance>(null);
 
-	useEffect(() => {
-		if (tokens.isAuthenticated()) {
-			// Redirect to home if already authenticated
-			navigate("/");
-		}
-	});
+  useEffect(() => {
+    if (tokens.isAuthenticated()) {
+      // Redirect to home if already authenticated
+      navigate("/");
+    }
+  });
 
-	useLayoutEffect(() => {
-		return () => {
-			turnstileRef.current?.remove();
-		};
-	}, []);
+  useLayoutEffect(() => {
+    return () => {
+      turnstileRef.current?.remove();
+    };
+  }, []);
 
-	const [loginData, setLoginData] = useReducer(
-		(state: any, newState: any) => ({ ...state, ...newState }),
-		{ username: "", password: "", showPassword: false },
-	);
+  const [loginData, setLoginData] = useReducer(
+    (state: any, newState: any) => ({ ...state, ...newState }),
+    { username: "", password: "", showPassword: false },
+  );
 
-	const handleSubmit = async (e: { preventDefault: () => void }) => {
-		e.preventDefault();
-		// Call login function from auth.js
-		accounts
-			.login(
-				loginData.username,
-				loginData.password,
-				turnstileRef.current?.getResponse()!,
-			)
-			.then((res) => {
-				// Show alert with status of login attempt
-				if (res === "success") {
-					toast({
-						title: `Logged in! Redirecting to dashboard...`,
-						status: "success",
-						isClosable: true,
-					});
-					navigate("/");
-				} else {
-					toast({
-						title: `${res}`,
-						status: "error",
-						isClosable: true,
-					});
-				}
-			})
-			.catch((err) => {
-				toast({
-					title: `${err}`,
-					status: "error",
-					isClosable: true,
-				});
-			});
-	};
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // Call login function from auth.js
+    accounts
+      .login(
+        loginData.username,
+        loginData.password,
+        turnstileRef.current?.getResponse()!,
+      )
+      .then((res) => {
+        // Show alert with status of login attempt
+        if (res === "success") {
+          toast({
+            title: `Logged in! Redirecting to dashboard...`,
+            status: "success",
+            isClosable: true,
+          });
+          navigate("/");
+        } else {
+          toast({
+            title: `${res}`,
+            status: "error",
+            isClosable: true,
+          });
+        }
+      })
+      .catch((err) => {
+        toast({
+          title: `${err}`,
+          status: "error",
+          isClosable: true,
+        });
+      });
+  };
 
-	return (
-		<Flex align={"center"} justify={"center"}>
-			<Stack spacing={8} mx={"auto"} maxW={"lg"} px={{ base: 0, md: 6 }}>
-				<Stack align={"center"}>
-					<Heading fontSize={"4xl"} textAlign="center">
-						Sign in to your account
-					</Heading>
-					<HStack spacing="1">
-						<Text>Or</Text>
-						<Link as={RouterLink} to="/signup" fontWeight="500">
-							create an account
-						</Link>
-					</HStack>
-				</Stack>
-				<Box rounded={"lg"} boxShadow={"lg"} p={8} pt={{ base: 4, md: 8 }}>
-					<form>
-						<Stack spacing={4}>
-							<FormControl id="username" isRequired>
-								<FormLabel>Username</FormLabel>
-								<Input
-									type="text"
-									onChange={(e) =>
-										setLoginData({
-											username: e.target.value,
-										})
-									}
-								/>
-							</FormControl>
-							<FormControl id="password" isRequired>
-								<FormLabel>Password</FormLabel>
-								<InputGroup>
-									<Input
-										type={loginData.showPassword ? "text" : "password"}
-										onChange={(e) =>
-											setLoginData({
-												password: e.target.value,
-											})
-										}
-									/>
-									<InputRightElement h={"full"}>
-										<Button
-											variant={"ghost"}
-											onClick={() =>
-												setLoginData({
-													showPassword: !loginData.showPassword,
-												})
-											}
-										>
-											{loginData.showPassword ? <ViewIcon /> : <ViewOffIcon />}
-										</Button>
-									</InputRightElement>
-								</InputGroup>
-							</FormControl>
-							<Stack spacing={10} alignItems="center">
-								{/* <Stack
+  return (
+    <Flex align={"center"} justify={"center"}>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} px={{ base: 0, md: 6 }}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign="center">
+            Sign in to your account
+          </Heading>
+          <HStack spacing="1">
+            <Text>Or</Text>
+            <Link as={RouterLink} to="/signup" fontWeight="500">
+              create an account
+            </Link>
+          </HStack>
+        </Stack>
+        <Box rounded={"lg"} boxShadow={"lg"} p={8} pt={{ base: 4, md: 8 }}>
+          <form>
+            <Stack spacing={4}>
+              <FormControl id="username" isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type="text"
+                  onChange={(e) =>
+                    setLoginData({
+                      username: e.target.value,
+                    })
+                  }
+                />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={loginData.showPassword ? "text" : "password"}
+                    onChange={(e) =>
+                      setLoginData({
+                        password: e.target.value,
+                      })
+                    }
+                  />
+                  <InputRightElement h={"full"}>
+                    <Button
+                      variant={"ghost"}
+                      onClick={() =>
+                        setLoginData({
+                          showPassword: !loginData.showPassword,
+                        })
+                      }>
+                      {loginData.showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} alignItems="center">
+                {/* <Stack
                   direction={{ base: "column", sm: "row" }}
                   align={"start"}
                   justify={"space-between"}
@@ -151,19 +150,19 @@ export default function Login() {
                     <Text>Forgot password?</Text>
                   </Link>
                 </Stack> */}
-								<Turnstile
-									ref={turnstileRef}
-									siteKey="0x4AAAAAAAI6ckchuGZipSqE"
-								/>
+                <Turnstile
+                  ref={turnstileRef}
+                  siteKey="0x4AAAAAACsYYSg7hk35U874"
+                />
 
-								<Button type="submit" onClick={handleSubmit}>
-									Sign in
-								</Button>
-							</Stack>
-						</Stack>
-					</form>
-				</Box>
-			</Stack>
-		</Flex>
-	);
+                <Button type="submit" onClick={handleSubmit}>
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: "0.0.0.0",
 		proxy: {
 			"/api": {
-				target: "http://0.0.0.0:3010",
-				changeOrigin: false,
-				secure: false,
-				ws: true,
+				target: "http://localhost:8080",
+				changeOrigin: true,
 			},
 		},
 	},
