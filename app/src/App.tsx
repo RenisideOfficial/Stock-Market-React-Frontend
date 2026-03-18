312b716f7fa02945ddf8e9874d10cf09fad00a45
RenisideOfficial <renrenxp1@gmail.com>
2026-03-18 06:32:11 +0100

    style: branding

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,72 +10,60 @@ const Leaderboard = lazy(() => import("./pages/Leaderboard"));
 import NotFound from "./pages/NotFound";
 
 export type Transaction = {
-	symbol: string;
-	purchasePrice: number;
-	quantity: number;
-	date: Date;
-	type: "buy" | "sell";
+  symbol: string;
+  purchasePrice: number;
+  quantity: number;
+  date: Date;
+  type: "buy" | "sell";
 };
 
 export type Position = {
-	symbol: string;
-	longName: string;
-	purchasePrice: number;
-	purchaseDate: Date;
-	quantity: number;
-	regularMarketPrice: number;
-	regularMarketPreviousClose: number;
-	regularMarketChangePercent: number;
+  symbol: string;
+  longName: string;
+  purchasePrice: number;
+  purchaseDate: Date;
+  quantity: number;
+  regularMarketPrice: number;
+  regularMarketPreviousClose: number;
+  regularMarketChangePercent: number;
 };
 
 function App() {
-	// Stock format: {symbol, count, price}
-	// const [selectedAction, setSelectedAction] = useState("buy");
-	// const [selelectedStock, setSelectedStock] = useState({
-	// 	symbol: "",
-	// 	price: 0,
-	// });
+  // Stock format: {symbol, count, price}
+  // const [selectedAction, setSelectedAction] = useState("buy");
+  // const [selelectedStock, setSelectedStock] = useState({
+  // 	symbol: "",
+  // 	price: 0,
+  // });
 
-	// const [selectedPrice, setSelectedPrice] = useState(0);
+  // const [selectedPrice, setSelectedPrice] = useState(0);
 
-	return (
-		<>
-			<Navbar />
-			<Container maxW="container.xl">
-				<Spacer h="10" />
-				<Box>
-					<Suspense fallback={<Spinner />}>
-						<Routes>
-							<Route path="/" element={<Dashboard />}></Route>
+  return (
+    <>
+      <Navbar />
+      <Container maxW="container.xl">
+        <Spacer h="10" />
+        <Box>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<Dashboard />}></Route>
 
-							<Route path="/login" element={<Login />}></Route>
+              <Route path="/login" element={<Login />}></Route>
 
-							<Route path="/signup" element={<Signup />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
 
-							<Route path="/leaderboard" element={<Leaderboard />}></Route>
+              <Route path="/leaderboard" element={<Leaderboard />}></Route>
 
-							<Route path="/stocks/:symbol" element={<StockView />}></Route>
+              <Route path="/stocks/:symbol" element={<StockView />}></Route>
 
-							{/* Add 404*/}
-							<Route path="*" element={<NotFound />}></Route>
-						</Routes>
-					</Suspense>
-				</Box>
-			</Container>
-			<Box textAlign="center" py="10">
-				<Text fontSize="sm" color="gray.500">
-					Built by{" "}
-					<Link href="https://spike.codes" fontWeight="bold">
-						Spike
-					</Link>{" "}
-					on{" "}
-					<Link href="https://github.com/spikecodes/stotra" fontWeight="bold">
-						GitHub
-					</Link>
-				</Text>
-			</Box>
-		</>
-	);
+              {/* Add 404*/}
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </Suspense>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,11 +1,11 @@
 import {
-	Box,
-	Flex,
-	Spacer,
-	Heading,
-	Text,
-	useBreakpointValue,
-	Link,
+  Box,
+  Flex,
+  Spacer,
+  Heading,
+  Text,
+  useBreakpointValue,
+  Link,
 } from "@chakra-ui/react";
 import PortfolioPreview from "../components/PortfolioPreview";
 import React from "react";
@@ -16,70 +16,69 @@ import tokens from "../services/tokens.service";
 import { Link as RouterLink } from "react-router-dom";
 
 export default function Dashboard() {
-	const isOnMobile = useBreakpointValue({ base: true, md: false });
+  const isOnMobile = useBreakpointValue({ base: true, md: false });
 
-	return (
-		<Box className="Dashboard">
-			<Flex direction={{ base: "column", md: "row" }} gap={5}>
-				<Box flex="0.75">
-					{tokens.isAuthenticated() ? (
-						<PortfolioPreview />
-					) : (
-						<>
-							<Heading as="h1" size="xl">
-								Stotra
-							</Heading>
-							<Text fontSize="lg">
-								<Link as={RouterLink} to="/signup">
-									Create an account
-								</Link>{" "}
-								or{" "}
-								<Link as={RouterLink} to="/login">
-									login
-								</Link>{" "}
-								to get started!
-							</Text>
-						</>
-					)}
-					{!isOnMobile && (
-						<>
-							<Spacer height={10} />
-							<Heading size="md">Stock Market News</Heading>
-							<Spacer height={2} />
-							<Newsfeed symbol={""} />
-						</>
-					)}
-				</Box>
-				<Box
-					flex="0.25"
-					borderWidth={{ base: 0, md: 1 }}
-					borderRadius="md"
-					p={{ base: 0, md: 3 }}
-					height={"fit-content"}
-				>
-					{tokens.isAuthenticated() ? (
-						<>
-							<PositionsList />
-							<Spacer h="3" />
-							<Watchlist />
-						</>
-					) : (
-						<Box>
-							<Heading as="h6" size="xs" textAlign={"center"}>
-								Sign in to view positions and watchlist
-							</Heading>
-						</Box>
-					)}
-				</Box>
-			</Flex>
-			{isOnMobile && (
-				<>
-					<Spacer height={10} />
-					<Heading size="md">Stock Market News</Heading>
-					<Spacer height={2} />
-					<Newsfeed symbol={""} />
-				</>
-			)}
-		</Box>
-	);
+  return (
+    <Box className="Dashboard">
+      <Flex direction={{ base: "column", md: "row" }} gap={5}>
+        <Box flex="0.75">
+          {tokens.isAuthenticated() ? (
+            <PortfolioPreview />
+          ) : (
+            <>
+              <Heading as="h1" size="xl">
+                Stock Mart
+              </Heading>
+              <Text fontSize="lg">
+                <Link as={RouterLink} to="/signup">
+                  Create an account
+                </Link>{" "}
+                or{" "}
+                <Link as={RouterLink} to="/login">
+                  login
+                </Link>{" "}
+                to get started!
+              </Text>
+            </>
+          )}
+          {!isOnMobile && (
+            <>
+              <Spacer height={10} />
+              <Heading size="md">Stock Market News</Heading>
+              <Spacer height={2} />
+              <Newsfeed symbol={""} />
+            </>
+          )}
+        </Box>
+        <Box
+          flex="0.25"
+          borderWidth={{ base: 0, md: 1 }}
+          borderRadius="md"
+          p={{ base: 0, md: 3 }}
+          height={"fit-content"}>
+          {tokens.isAuthenticated() ? (
+            <>
+              <PositionsList />
+              <Spacer h="3" />
+              <Watchlist />
+            </>
+          ) : (
+            <Box>
+              <Heading as="h6" size="xs" textAlign={"center"}>
+                Sign in to view positions and watchlist
+              </Heading>
+            </Box>
+          )}
+        </Box>
+      </Flex>
+      {isOnMobile && (
+        <>
+          <Spacer height={10} />
+          <Heading size="md">Stock Market News</Heading>
+          <Spacer height={2} />
+          <Newsfeed symbol={""} />
+        </>
+      )}
+    </Box>
+  );
 }
